fe2146e82a59da9c154b718d33cbd4e5762aeec9
hamza5645 <hamzaosama5645@outlook.com>
2024-06-13 00:20:22 +0300

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Hamza Osama on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
